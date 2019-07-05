2ad929c7e1591c6364fa0652d07dbc46cd25f7fb
rnranalysis <43582849+rnranalysis@users.noreply.github.com>
2019-07-05 08:30:12 -0400

    Update REsolve_AZORult.py

--- REsolve_AZORult.py
+++ REsolve_AZORult.py
@@ -1,6 +1,5 @@
 # CURRENTLY WORKING ON ASSIGNING LABELS TO THE GLOBAL VARS
 
-
 # REsolve GetProcAddress -> Global Vars
 #@author James Haughom
 #@category _NEW_
@@ -11,20 +10,14 @@
 import re
 from java.lang import String
 
-def commentTest():
-	listing = currentProgram.getListing()
-	codeUnit = listing.getCodeUnitAt(toAddr("0x00415e68"))
-	codeUnit.setComment(codeUnit.EOL_COMMENT, "TEST COMMENT!")
+listing = currentProgram.getListing()
 
 def comment(api, addr):
-	listing = currentProgram.getListing()
 	codeUnit = listing.getCodeUnitAt(addr)
 	codeUnit.setComment(codeUnit.EOL_COMMENT, api)
 
-
 def getSymbol(api, addr):
 	createSymbol(toAddr(addr), api, True)
-
 
 def getString(addr):
 	byte = 1
@@ -38,30 +31,30 @@ def getString(addr):
 	return(string)
 
 def main():
-	commentTest()
-	listing = currentProgram.getListing()
-	loc = 0x00404e98 #GetProcAddress
+	getprocaddr = 0x00404e98 #GetProcAddress
 
-	refs = getReferencesTo(toAddr(loc))
+	refs = getReferencesTo(toAddr(getprocaddr))
 
-	for r in refs:
+	for ref in refs:
 
-		callee = r.getFromAddress()
+		callee = ref.getFromAddress()
 		inst = getInstructionAt(callee)
 		if "CALL" in inst.toString():
 			push_eax = getInstructionBefore(inst)
 			if "PUSH EAX" in push_eax.toString():
 				mov_eax_ebx = getInstructionBefore(push_eax)
 				if "MOV EAX" in mov_eax_ebx.toString():
-					#a = re.findall('0x[a-fA-F0-9]{6,8}', mov_eax_ebx.toString())
 					push_api = getInstructionBefore(mov_eax_ebx)
 					push_api = push_api.toString()
 					match = re.findall('0x[a-fA-F0-9]{6,8}', push_api)
 					if match:
 						api = getString(toAddr(match[0]))
 						print('[+] API REsolved: ' + api + ' at ' + match[0])
-						#a = re.findall('0x[a-fA-F0-9]{6,8}', mov_eax_ebx.toString())
-						#getSymbol(api, a[0])
+						mov_eax_api = getInstructionAfter(inst)
+						if "MOV" and "EAX" in mov_eax_api.toString():
+							symbol_addr = re.findall('0x[a-fA-F0-9]{6,8}', mov_eax_api.toString())
+							if symbol_addr:
+								getSymbol(api, symbol_addr[0])
 					else:
 						continue
 
