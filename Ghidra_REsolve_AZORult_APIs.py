af7885fd2a55b331efebb4001f076c67a6edc5d5
rnranalysis <43582849+rnranalysis@users.noreply.github.com>
2019-07-06 15:33:02 -0400

    Update Ghidra_REsolve_AZORult_APIs.py

--- Ghidra_REsolve_AZORult_APIs.py
+++ Ghidra_REsolve_AZORult_APIs.py
@@ -1,21 +1,14 @@
 # REsolve GetProcAddress -> Global Vars
-#@author James Haughom
+#@author James Haughom Jr
 #@category _NEW_
 #@keybinding 
 #@menupath 
 #@toolbar 
 
+
 import re
-from java.lang import String
 
 listing = currentProgram.getListing()
-
-def comment(api, addr):
-	codeUnit = listing.getCodeUnitAt(addr)
-	codeUnit.setComment(codeUnit.EOL_COMMENT, api)
-
-def getSymbol(api, addr):
-	createSymbol(toAddr(addr), api, True)
 
 def getString(addr):
 	string = ''
@@ -26,6 +19,10 @@ def getString(addr):
 		addr = addr.add(1)
 		byte = getByte(addr)
 	return(string)
+
+def getSymbol(api, addr):
+	createSymbol(toAddr(addr), api, True)
+
 
 def main():
 	getprocaddr = 0x00404e98 #GetProcAddress
@@ -42,18 +39,15 @@ def main():
 				mov_eax_ebx = getInstructionBefore(push_eax)
 				if "MOV EAX" in mov_eax_ebx.toString():
 					push_api = getInstructionBefore(mov_eax_ebx)
-					push_api = push_api.toString()
-					match = re.findall('0x[a-fA-F0-9]{6,8}', push_api)
+					match = re.findall('0x[a-fA-F0-9]{6,8}', push_api.toString())
 					if match:
 						api = getString(toAddr(match[0]))
-						print('[+] API REsolved: ' + api + ' at ' + match[0])
 						mov_eax_api = getInstructionAfter(inst)
 						if "MOV" and "EAX" in mov_eax_api.toString():
 							symbol_addr = re.findall('0x[a-fA-F0-9]{6,8}', mov_eax_api.toString())
 							if symbol_addr:
 								getSymbol(api, symbol_addr[0])
-					else:
-						continue
 
 
 main()
+
